5556cae51179441ddc133ed9c54a887cf935974a
Kolja Kleinschmidt <kkleinsc@c1s14d66.42wolfsburg.de>
2023-01-02 17:46:03 +0100

    Wrong Main output

--- test/main.c
+++ test/main.c
@@ -13,13 +13,10 @@ int run_test(char *name, int (*test)())
 		write(1, KNRM, 4);
 		return (0);
 	}
-	else
-	{
-		write(1, KGRN, 5);
-		write(1, "OK\n", 3);
-		write(1, KNRM, 4);
-		return (1);
-	}
+	write(1, KGRN, 5);
+	write(1, "OK\n", 3);
+	write(1, KNRM, 4);
+	return (1);
 }
 
 /*
@@ -31,6 +28,6 @@ int main()
 	// write(2, "Failed\n", 7);
 	// write(2, KNRM, 4);
 	if (!run_test("testRest", &testRest))
-		return (0);
-	return (1);
+		return (1);
+	return (0);
 }

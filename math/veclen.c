54b88cdc971df5fc64f29f2ae46118a367dc4232
Kolja Kleinschmidt <kkleinsc@c1s14d66.42wolfsburg.de>
2023-01-03 13:14:15 +0100

    Network

---
+++ math/veclen.c
@@ -0,0 +1,14 @@
+
+#include "operations.h"
+
+double vec3d_len(vec3d *vector)
+{
+	double	a;
+	double	b;
+	double	c;
+
+	a = pow(vector->x, 2.0);
+	b = pow(vector->y, 2.0);
+	c = pow(vector->z, 2.0);
+	a = sqrt
+}

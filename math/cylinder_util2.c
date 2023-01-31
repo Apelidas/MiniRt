c5e9ba5654fbf3b029a4fb97b66cbb7142c5d60b
Kolja Kleinschmidt <kkleinsc@c2s18d116.42wolfsburg.de>
2023-01-31 16:20:49 +0100

    tried copying from main failed

--- math/cylinder_util2.c
+++ math/cylinder_util2.c
@@ -1,15 +1,19 @@
 #include "operations.h"
 
+/*
+checks if a ray and a plane have a point of intesrsection
+*/
 static int	touch(t_ray *ray, t_plane	*plane)
 {
-	double	dot;
-
-	dot = vec3d_dot(ray->dir, plane->norm);
-	if (cmp_d(dot, 0))
+	if (vec3d_dot(ray->dir, plane->norm) == 0)
 		return (0);
 	return (1);
 }
 
+/*
+returns the point of intersection of a plane and a ray.
+returns NULL if there is none
+*/
 t_vec3d	*cyl_plane_ray(t_ray *ray, t_plane *plane)
 {
 	double	t;
@@ -22,13 +26,11 @@ t_vec3d	*cyl_plane_ray(t_ray *ray, t_plane *plane)
 	inter = create_vec3d(0, 0, 0);
 	if (!inter)
 		return (NULL);
-	print_vec3d(ray->dir);
 	t = vec3d_dot(plane->origin, plane->norm)
 		- vec3d_dot(ray->origin, plane->norm);
 	t = t / vec3d_dot(ray->dir, plane->norm);
 	inter->x = ray->origin->x + t * ray->dir->x;
 	inter->y = ray->origin->y + t * ray->dir->y;
 	inter->z = ray->origin->z + t * ray->dir->z;
-	print_vec3d(inter);
 	return (inter);
 }
